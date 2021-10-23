5d3545052cc5af84e56c853cf37df507211531dc
saber2pr <saber2pr@gmail.com>
2021-10-23 17:49:19 +0800

    chore: update

--- cli.js
+++ cli.js
@@ -3,5 +3,5 @@ const create = require('@saber2pr/cli').default
 create({
   'name': 'cli-git',
   'libRoot': __dirname,
-  "releaseZipUrl": "https://github.com/saber2pr-forks/sa-git.git"
+  "releaseZipUrl": "https://github.com/saber2pr-forks/sa-git/archive/refs/heads/master.zip"
 })
